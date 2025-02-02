a7cc53ee382c807434a6dbe71b6f366cb04fc277
Muhammed Mustafa Geldi <swankydata9@gmail.com>
2025-02-02 14:10:46 +0300

    Initial Commit

---
+++ TwistTale/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwistTale
+//
+//  Created by Muhammed Mustafa Geldi on 2.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TwistTaleTests/TwistTaleTests.swift
@@ -0,0 +1,17 @@
+//
+//  TwistTaleTests.swift
+//  TwistTaleTests
+//
+//  Created by Muhammed Mustafa Geldi on 2.02.2025.
+//
+
+import Testing
+@testable import TwistTale
+
+struct TwistTaleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
